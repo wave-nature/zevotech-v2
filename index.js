5a7b6fddabbf7e03316eeaee4a73dacbcadcbe99
wave-nature <vs1010582@gmail.com>
2024-02-03 14:36:29 +0530

    80% complete

---
+++ index.js
@@ -0,0 +1,31 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://plpyfutennmvnfjedkpy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBscHlmdXRlbm5tdm5mamVka3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5NTAwMTQsImV4cCI6MjAyMjUyNjAxNH0.iHeInXtPuJ0n61dHjDB2zPJaGIoDHlet1I-V_jCn5fE"
+);
+
+const contactFormBtn = document.getElementById("send-message");
+
+async function contactFormHandler(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  await supabase.from("contacts").insert([
+    {
+      name,
+      email,
+      message,
+    },
+  ]);
+
+  alert("We will get back to you soon!");
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+}
+
+contactFormBtn.addEventListener("click", contactFormHandler);
